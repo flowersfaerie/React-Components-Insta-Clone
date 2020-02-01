14394127e0597bcae13ab05b9679b72eda805b56
Sarah Flowers <flowersfaerie@gmail.com>
2020-02-01 11:57:09 -0800

    Started the Insta-Clone project

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+import {PostsPage} from "./Components/PostsContainer/PostsPage";
+import {CommentSection} from "./Components/SearchBar/SearchBarContainer"
+import * as postData from "./dummy-data"
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
 
-const App = () => {
+const App = (props) => {
   // set up state for the dummy data and pass to your PostsPage
   return (
     <div className="App">
